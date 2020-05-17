fe7413eee34eaf49f249bd06d5628aaa54542a34
Marco Nicola <marconicola@disroot.org>
2020-05-17 14:06:44 +0200

    Add copyright notice to pytorch generate_fixtures.py

--- pytorch/testdata/generate_fixtures.py
+++ pytorch/testdata/generate_fixtures.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# Copyright 2020 NLP Odyssey Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 import torch
 
 FLOAT_DTYPES = [
