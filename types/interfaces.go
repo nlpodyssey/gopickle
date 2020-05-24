c62ea9a124c6497f74ce92bbdba1eb71c018bd3f
Marco Nicola <marconicola@disroot.org>
2020-05-24 16:00:19 +0200

    Add documentation to types.Callable

--- types/interfaces.go
+++ types/interfaces.go
@@ -4,7 +4,15 @@
 
 package types
 
+// Callable is implemented by any value that can be directly called to get a
+// new value.
+//
+// It is usually implemented by Python-like functions (returning a value
+// given some arguments), or classes (typically returning an instance given
+// some constructor arguments).
 type Callable interface {
+	// Call mimics a direct invocation on a Python value, such as a function
+	// or class (constructor).
 	Call(args ...interface{}) (interface{}, error)
 }
 
