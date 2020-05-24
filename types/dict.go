652c8e41c37d646d2ba53305dae32090c3e1c8cf
Marco Nicola <marconicola@disroot.org>
2020-05-24 17:25:20 +0200

    Add documentation to Dict type

--- types/dict.go
+++ types/dict.go
@@ -4,28 +4,40 @@
 
 package types
 
+// DictSetter is implemented by any value that exhibits a dict-like behaviour,
+// allowing arbitrary key/value pairs to be set.
 type DictSetter interface {
-	Set(k, v interface{})
+	Set(key, value interface{})
 }
 
+// Dict represents a Python "dict" (builtin type).
 type Dict map[interface{}]interface{}
 
 var _ DictSetter = &Dict{}
 
+// NewDict makes and returns a new empty Dict.
 func NewDict() *Dict {
 	d := make(Dict)
 	return &d
 }
 
-func (d *Dict) Set(k, v interface{}) {
-	(*d)[k] = v
+// Set sets to into the Dict the given key/value pair.
+//
+// If the key is already present, its associated value is replaced with the
+// new one.
+func (d *Dict) Set(key, value interface{}) {
+	(*d)[key] = value
 }
 
-func (d *Dict) Get(k interface{}) (interface{}, bool) {
-	value, ok := (*d)[k]
+// Get returns the value associated with the given key (if any), and whether
+// the key is present or not.
+func (d *Dict) Get(key interface{}) (interface{}, bool) {
+	value, ok := (*d)[key]
 	return value, ok
 }
 
+// Len returns the length of the Dict, that is, the amount of key/value pairs
+// contained by the Dict.
 func (d *Dict) Len() int {
 	return len(*d)
 }
