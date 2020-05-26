532a682f51929be4a2ece40a1f20670a3c2df89e
Marco Nicola <marconicola@disroot.org>
2020-05-26 18:20:09 +0200

    Modify FrozenSet implementation avoiding confusion with Set
    and providing documentation

--- types/frozenset.go
+++ types/frozenset.go
@@ -4,14 +4,32 @@
 
 package types
 
-type FrozenSet struct {
-	*Set
-}
+// FrozenSet represents a Python "frozenset" (builtin type).
+//
+// It is implemented in Go as a map with empty struct values; the actual set
+// of generic "interface{}" items is thus represented by all the keys.
+type FrozenSet map[interface{}]frozenSetEmptyStruct
 
-func NewFrozenSet() *FrozenSet {
-	return &FrozenSet{NewSet()}
-}
+type frozenSetEmptyStruct struct{}
 
+// NewFrozenSetFromSlice makes and returns a new FrozenSet initialized
+// with the elements of the given slice.
 func NewFrozenSetFromSlice(slice []interface{}) *FrozenSet {
-	return &FrozenSet{NewSetFromSlice(slice)}
+	f := make(FrozenSet, len(slice))
+	for _, item := range slice {
+		f[item] = frozenSetEmptyStruct{}
+	}
+	return &f
+}
+
+// Len returns the length of the FrozenSet.
+func (f *FrozenSet) Len() int {
+	return len(*f)
+}
+
+// Has returns whether the given value is present in the FrozenSet (true)
+// or not (false).
+func (f *FrozenSet) Has(v interface{}) bool {
+	_, ok := (*f)[v]
+	return ok
 }
