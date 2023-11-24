cd99eb412a045d3251e23fbe251c59fc7f39f834
Sebastien Binet <binet@cern.ch>
2023-11-24 15:53:55 +0100

    types: add Stringer implementation for Tuple
    Signed-off-by: Sebastien Binet <binet@cern.ch>

--- types/tuple.go
+++ types/tuple.go
@@ -4,6 +4,11 @@
 
 package types
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Tuple []interface{}
 
 func NewTupleFromSlice(slice []interface{}) *Tuple {
@@ -17,4 +22,23 @@ func (t *Tuple) Get(i int) interface{} {
 
 func (t *Tuple) Len() int {
 	return len(*t)
+}
+
+func (t *Tuple) String() string {
+	if t == nil {
+		return "nil"
+	}
+	o := new(strings.Builder)
+	o.WriteString("(")
+	for i, v := range *t {
+		if i > 0 {
+			o.WriteString(", ")
+		}
+		fmt.Fprintf(o, "%v", v)
+	}
+	if t.Len() == 1 {
+		o.WriteString(",")
+	}
+	o.WriteString(")")
+	return o.String()
 }
