77da449d2f105dab6630414811627ee2bfb9013a
Sebastien Binet <binet@cern.ch>
2023-11-27 15:02:34 +0100

    types: add Stringer implementation for Dict
    Signed-off-by: Sebastien Binet <binet@cern.ch>

--- types/dict_test.go
+++ types/dict_test.go
@@ -1,6 +1,14 @@
+// Copyright 2023 NLP Odyssey Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package types
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
 
 func TestDictCall(t *testing.T) {
 	d := NewDict()
@@ -12,5 +20,29 @@ func TestDictCall(t *testing.T) {
 	expected := "bar"
 	if actual != expected {
 		t.Errorf("expected %v, actual: %v", expected, actual)
+	}
+}
+
+func TestDictStringer(t *testing.T) {
+	dct := NewDict()
+	dct.Set("empty", NewDict())
+	dct.Set("one", "un")
+	dct.Set("two", "deux")
+	sub := NewDict()
+	sub.Set("eins", "one")
+	sub.Set("zwei", []string{"two", "deux"})
+	sub.Set(2, []int{2 * 2, 2 * 3, 2 * 4})
+	dct.Set("sub", sub)
+
+	buf := new(bytes.Buffer)
+	fmt.Fprintf(buf, "%v", dct)
+
+	var (
+		got  = buf.String()
+		want = "{empty: {}, one: un, two: deux, sub: {eins: one, zwei: [two deux], 2: [4 6 8]}}"
+	)
+
+	if got != want {
+		t.Fatalf("got= %q\nwant=%q", got, want)
 	}
 }
